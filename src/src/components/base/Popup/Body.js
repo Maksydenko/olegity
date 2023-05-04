f5861b310067ab056c0661dcbf78caf0bf2c874d
Maksym Davydenko <maksydenko@gmail.com>
2023-05-04 16:09:21 +0300

    Minor fixes

--- src/src/components/base/Popup/Body.js
+++ src/src/components/base/Popup/Body.js
@@ -1,11 +1,14 @@
+import { useRef } from "react";
+
 const Body = ({ children, onClick }) => {
+  const popupBoxRef = useRef();
   const handleClick = ({ target }) =>
-    !target.closest(".popup__box") && onClick();
+    !popupBoxRef.current.contains(target) && onClick();
 
   return (
     <div className="popup__body" onClick={handleClick}>
       <div className="popup__content">
-        <div className="popup__box">
+        <div className="popup__box" ref={popupBoxRef}>
           <button className="popup__cross" onClick={onClick}></button>
           {children}
         </div>
