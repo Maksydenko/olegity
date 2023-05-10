2174543b5fc909790c21a29eeee72843bbcc10e4
Maksym Davydenko <maksydenko@gmail.com>
2023-05-10 18:35:47 +0300

    Minor fixes

--- src/src/components/screens/404/404.js
+++ src/src/components/screens/404/404.js
@@ -7,17 +7,15 @@ const Page404 = () => {
   const { t } = useTranslation();
 
   return (
-    <main className="404-page">
-      <div className="page-not-found">
-        <div className="page-not-found__container">
-          <h1 className="page-not-found__title">404</h1>
-          <h2 className="page-not-found__label">{t("page-not-found")}</h2>
-          <p className="page-not-found__redirect-homepage">
-            {t("redirect-homepage")} {time}
-          </p>
-        </div>
+    <div className="page-not-found">
+      <div className="page-not-found__container">
+        <h1 className="page-not-found__title">404</h1>
+        <h2 className="page-not-found__label">{t("page-not-found")}</h2>
+        <p className="page-not-found__redirect-homepage">
+          {t("redirect-homepage")} {time}
+        </p>
       </div>
-    </main>
+    </div>
   );
 };
 
--- src/src/pages/404/404.js
+++ src/src/pages/404/404.js
@@ -1,5 +1,9 @@
-import PageNotFound from "@screens/404/404";
+import NotFoundPage from "@screens/404/404";
 
-const Page404 = () => <PageNotFound />;
+const Page404 = () => (
+  <main className="404-page">
+    <NotFoundPage />
+  </main>
+);
 
 export default Page404;
