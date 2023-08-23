ec450d5730bbeef08e2e65cbbee793e6a68a4e0c
Maksym Davydenko <maksydenko@gmail.com>
2023-08-23 20:36:01 +0300

    New popup and style fix on discography page

--- src/src/components/base/Popup/Popup.js
+++ src/src/components/base/Popup/Popup.js
@@ -1,55 +1,65 @@
-import Transition from "@components/base//Transition/Transition";
-import Body from "./Body";
+import { useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
-import { useScrollLock } from "@hooks/useScrollLock";
-
-import { handleClassName } from "@utils/className.util";
-
-const Popup = ({
-  className,
-  modifier,
-  children,
-  button,
-  isActive,
-  setIsActive,
-}) => {
-  const { isScrollLocked, setIsScrollLocked } = useScrollLock();
+const Popup = ({ className, children, button, isActive, setIsActive }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const isActiveIsUndefined = typeof isActive === "undefined";
 
-  const handleClick = () => {
+  const handleOpen = () => {
     if (!isActiveIsUndefined && setIsActive) {
-      setIsActive(!isActive);
+      setIsActive(true);
     } else {
-      setIsScrollLocked(!isScrollLocked);
+      setIsOpen(true);
     }
   };
 
-  const handleDisablePopup = () => {
+  const handleClose = () => {
     if (!isActiveIsUndefined && setIsActive) {
       setIsActive(false);
     } else {
-      setIsScrollLocked(false);
+      setIsOpen(false);
     }
   };
 
-  const condition =
-    !isActiveIsUndefined && setIsActive ? isActive : isScrollLocked;
-
-  const modifiedClassName = handleClassName(
-    !!modifier,
-    `${className}__popup`,
-    modifier
-  );
+  const condition = !isActiveIsUndefined && setIsActive ? isActive : isOpen;
 
   return (
-    <div className={`${modifiedClassName} popup`}>
-      <button className="popup__button" onClick={handleClick}>
-        {button}
-      </button>
-      <Transition condition={condition} className="popup">
-        <Body onClick={handleDisablePopup}>{children}</Body>
+    <>
+      <div className={`${className}__popup popup`}>
+        <button className="popup__button" onClick={handleOpen}>
+          {button}
+        </button>
+      </div>
+      <Transition appear show={condition}>
+        <Dialog className={`${className}__popup popup`} onClose={handleClose}>
+          <div className="popup__body">
+            <Transition.Child
+              className="popup__bg"
+              enter="popup__enter"
+              enterFrom="popup__enter-from"
+              enterTo="popup__enter-to"
+              leave="popup__leave"
+              leaveFrom="popup__leave-from"
+              leaveTo="popup__leave-to"
+            />
+            <Transition.Child
+              className="popup__content"
+              enter="popup__enter popup__enter_content"
+              enterFrom="popup__enter-from popup__enter-from_content"
+              enterTo="popup__enter-to popup__enter-to_content"
+              leave="popup__leave popup__leave_content"
+              leaveFrom="popup__leave-from popup__leave-from_content"
+              leaveTo="popup__leave-to popup__leave-to_content"
+            >
+              <Dialog.Panel className="popup__box">
+                <div className="popup__children">{children}</div>
+                <button className="popup__cross" onClick={handleClose}></button>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
       </Transition>
-    </div>
+    </>
   );
 };
 
