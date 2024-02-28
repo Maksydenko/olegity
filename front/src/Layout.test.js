4d0c1fa46a9ac2af95f1f7533414f957884a67d8
Maksym Davydenko <maksydenko@gmail.com>
2024-02-28 22:02:07 +0200

    Layout.test fix

--- front/src/Layout.test.js
+++ front/src/Layout.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 
-import Layout from '@components/layout/Layout';
+import Layout from "components/layout/Layout";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<Layout />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
