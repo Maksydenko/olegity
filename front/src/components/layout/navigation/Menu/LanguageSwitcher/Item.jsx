6cedfb7274b5f5eba9327f98c4a079a64ae50e9c
Maksym Davydenko <maksydenko@gmail.com>
2024-03-03 17:40:19 +0200

    Languages. Fix

--- front/src/components/layout/navigation/Menu/LanguageSwitcher/Item.jsx
+++ front/src/components/layout/navigation/Menu/LanguageSwitcher/Item.jsx
@@ -1,20 +1,35 @@
+import { useCallback, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 const Item = ({ language: { value, path }, onClick }) => {
   const { i18n } = useTranslation();
   const { language } = i18n;
 
+  const isCurrentLanguage = path === language;
+
+  const setHTMLLang = useCallback(() => {
+    const htmlElement = document.querySelector("html");
+    htmlElement.setAttribute("lang", path);
+  }, [path]);
+
   const handleChangeLanguage = () => {
     i18n.changeLanguage(path);
+    setHTMLLang();
     onClick();
   };
+
+  useEffect(() => {
+    if (language !== "en") {
+      setHTMLLang();
+    }
+  }, [language, setHTMLLang]);
 
   return (
     <li className="language-switcher__item">
       <button
         className="language-switcher__btn"
         type="button"
-        disabled={path === language}
+        disabled={isCurrentLanguage}
         onClick={handleChangeLanguage}
       >
         {value}
