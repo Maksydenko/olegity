8b09953b1e0f9e3ab9ef3f75df828ef6883ba3e9
Maksym Davydenko <maksydenko@gmail.com>
2024-03-10 21:30:16 +0200

    Layout. Fixes

--- front/src/components/layout/navigation/Menu/Items/Item.jsx
+++ front/src/components/layout/navigation/Menu/Items/Item.jsx
@@ -1,30 +1,22 @@
-"use client";
-
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 // import { useTranslation } from "next-i18next";
 import clsx from "clsx";
 
 const Item = ({ link: { value, path }, onClick }) => {
-  // const { pathname } = useLocation();
+  const pathname = usePathname();
   // const { t } = useTranslation();
 
-  // const isActive = path === pathname;
+  const isActive = path === pathname;
 
   return (
-    <li
-      className={clsx(
-        "menu__item"
-        // isActive && "menu__item_active"
-      )}
-    >
+    <li className={clsx("menu__item", isActive && "menu__item_active")}>
       <Link
         className="menu__link"
         href={path}
-        onClick={onClick}
-        // {...(!isActive && {
-        //   to: path,
-        //   onClick,
-        // })}
+        {...(!isActive && {
+          onClick,
+        })}
       >
         {value}
       </Link>
--- front/src/components/layout/navigation/Navigation/Items/Item.jsx
+++ front/src/components/layout/navigation/Navigation/Items/Item.jsx
@@ -1,24 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 // import { useTranslation } from "next-i18next";
 import clsx from "clsx";
 
 const Item = ({ link: { value, path } }) => {
-  // const { pathname } = useLocation();
+  const pathname = usePathname();
   // const { t } = useTranslation();
 
-  // const isActive = path === pathname;
+  const isActive = path === pathname;
 
   return (
     <li className="navigation__item">
       <Link
         className={clsx(
-          "navigation__link"
-          // isActive && "navigation__link_active"
+          "navigation__link",
+          isActive && "navigation__link_active"
         )}
         href={path}
-        // {...(!isActive && {
-        // to: path,
-        // })}
       >
         {value}
       </Link>
