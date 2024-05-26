d448d73e5830c7ba7345670f24e1980d1ee4e99b
Nyaruko <kosenkoroma123@gmail.com>
2024-05-26 20:26:27 +0200

    Fix

--- back/olegity/obj/Release/net8.0/olegity.AssemblyInfo.cs
+++ back/olegity/obj/Release/net8.0/olegity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("olegity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9eb2d3a34176f2d513781f936c09716a42fc766")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("olegity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("olegity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
