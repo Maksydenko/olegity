032da890e1c11ed2df25d293a334420e956808ea
Maksym Davydenko <maksydenko@gmail.com>
2024-06-04 20:22:15 +0300

    AxiosInstanse. Refactoring

--- front/src/services/axiosInstance.ts
+++ front/src/services/axiosInstance.ts
@@ -1,7 +1,10 @@
 import axios, { AxiosInstance } from "axios";
 
+const BACK_URI = "http://localhost:8000";
+const BACK_API_URI = `${BACK_URI}/api`;
+
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: BACK_API_URI,
 });
 
 export default axiosInstance;
